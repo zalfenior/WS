e76a9dbfdb7054f606336bf6e7c1a5ef0ca7af55
Mike <mhostler@unomaha.edu>
2018-11-23 18:14:10 -0600

    PR calculations finished, not tested

--- WebSearch/src/GraphNode.java
+++ WebSearch/src/GraphNode.java
@@ -5,14 +5,16 @@ public class GraphNode{
   private int id;
   private String name = "N/A";
   private int degree = 0;
-  private double pageRank;
+  private double [] pageRank;
   private String printer = "";
   public ConcurrentLinkedQueue<Edge> linkTo = new ConcurrentLinkedQueue<Edge>();
 
   
-  public GraphNode(int init) {
+  public GraphNode(int init, int iter) {
     id = init;
+    pageRank = new double[iter];
   }
+  
   public int getID() { return id; }
   public void setID(int res) { id = res; }
 
@@ -26,8 +28,8 @@ public class GraphNode{
   public void setPrint(String neo) { printer = neo; }
   public void addPrint(String x) { printer += x + "\n"; } //convert float value to string before call or in method
   
-  public double getpageRank() { return pageRank; }
-  public void setpageRank(double neo) { pageRank = neo; }
+  public double getpageRank(int index) { return pageRank[index]; }
+  public void setpageRank(int index, double neo) { pageRank[index] = neo; }
   
   public ConcurrentLinkedQueue<Edge> getQueue() { return linkTo; }
   public void setQueue( ConcurrentLinkedQueue<Edge> queue ) { linkTo = queue; }
